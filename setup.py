fbec0a13961f9bf9e1e6a35e1627cbb7bc563b4a
Bartosz T <bartosz@telenczuk.pl>
2015-08-24 15:12:31 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+from distutils.core import setup
+
+setup(name='bashflow',
+      version='0.1',
+      description='Bash-based workflow control',
+      author='Bartosz Telenczuk',
+      author_email='muchatel@poczta.fm',
+      url='https://www.github.com/btel/bashflow',
+      packages=['bashflow'],
+     )
