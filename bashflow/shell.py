16329b960ae626b070b7d1510ce2dac71e6ba454
Bartosz T <bartosz@telenczuk.pl>
2015-12-29 00:35:27 +0100

    allow to use bash parameters with mutliple files in rules

--- bashflow/shell.py
+++ bashflow/shell.py
@@ -15,23 +15,34 @@ def parse_rule(line):
     m = re.match(regexpr, line)
     if m:
         inputs, outputs = m.groups()
-        inputs = inputs.split(',')
-        outputs = outputs.split(',')
+        inputs = re.findall('[^ ,]+', inputs)
+        outputs = re.findall('[^ ,]+', outputs)
         return inputs, outputs
     else:
         raise NotARule('not a rule')
 
 def make_if_statement(inputs, outputs):
     conditions = []
-    inputs_exist = ""
-    for inp in inputs:
-        inputs_exist += '[ ! -e {} ] && echo "Input file {} does not exist" >&2 && exit\n'.format(inp, inp)
+    filedefs = 'INPUTFILES=`echo {}`\n'.format(" ".join(inputs))
+    filedefs += 'OUTPUTFILES=`echo {}`\n'.format(" ".join(outputs))
+    inputs_exist = 'for file in $INPUTFILES; do [ ! -e "$file" ] && echo "Input file $file does not exist" >&2 && exit; done\n'
 
-    for outp in outputs:
-        for inp in inputs:
-            conditions.append('[ {} -nt {} ]'.format(inp, outp))
-    if_stmt = "if {}\nthen\n".format(" | ".join(conditions))
-    return inputs_exist + if_stmt
+    conditions = """
+    runblock=0
+    for inp in $INPUTFILES
+    do
+        for out in $OUTPUTFILES
+        do
+            if [ "$inp" -nt "$out" ]
+            then
+                runblock=1
+            fi
+        done
+    done
+    """
+    if_stmt = 'if [ "$runblock" -eq 1 ]\nthen\n'
+
+    return filedefs + inputs_exist + conditions + if_stmt
 
 
 
@@ -54,11 +65,14 @@ with file(args.script) as fid:
                 if open_statement:
                     generated_scripts.write('fi\n')
                     open_statement = False
-                line = make_if_statement(inputs, outputs)
+                outp_line = make_if_statement(inputs, outputs)
                 open_statement = True
+                outp_line += 'echo "Running rule: {}"\n'.format(line[1:])
             except NotARule:
-                pass
-        generated_scripts.write(line) 
+                outp_line = line
+        else:
+            outp_line = line
+        generated_scripts.write(outp_line) 
 
 if open_statement:
     generated_scripts.write('fi\n')
